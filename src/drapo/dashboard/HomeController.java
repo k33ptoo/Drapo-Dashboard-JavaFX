ad03e21d066f054f93c2234c1b749036a021f9b0
Amos Chepchieng <akchieng@gmail.com>
2018-04-17 14:42:20 +0300

    Added files via upload

---
+++ src/drapo/dashboard/HomeController.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package drapo.dashboard;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.NodeOrientation;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author oXCToo
+ */
+public class HomeController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+      @FXML
+    private VBox pnl_scroll;
+    
+    @FXML
+    private void handleButtonAction(MouseEvent event) {        
+       refreshNodes();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+         refreshNodes();
+    }    
+    
+    private void refreshNodes()
+    {
+        pnl_scroll.getChildren().clear();
+        
+        Node [] nodes = new  Node[15];
+        
+        for(int i = 0; i<10; i++)
+        {
+            try {
+                nodes[i] = (Node)FXMLLoader.load(getClass().getResource("Item.fxml"));
+               pnl_scroll.getChildren().add(nodes[i]);
+                
+            } catch (IOException ex) {
+                Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+           
+        }  
+    }
+    
+}
